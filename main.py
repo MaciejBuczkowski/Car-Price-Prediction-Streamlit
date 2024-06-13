3b13ef2057ca034ab2a6512c48bdb3b653b28e0c
MaciejBuczkowski <mpbuczkowski@gmail.com>
2024-06-13 17:17:40 +0200

    model prediction implemented
    TODO: plot results

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from link import retrive_data
 from sklearn.preprocessing import LabelEncoder
 import joblib
 import psycopg2
+import pandas as pd
+from valuate import prediction
 
 
 db = st.connection('postgresql',type='sql')
@@ -11,6 +13,8 @@ st.title('Car Price Evaluation')
 
 mode = st.selectbox('Choose your mode, Link or Manual entry?:',['No Selection','Link','Manual entry'])
 
+pred = []
+
 if mode == 'Link':
     link = st.text_input('Link')
     click = st.button('Enter')
@@ -18,6 +22,8 @@ if mode == 'Link':
     if click:
         
         data = retrive_data(link)
+        
+        pred = prediction(data)
         
         st.write(data)
     
@@ -43,7 +49,7 @@ elif mode == 'Manual entry':
     m = st.number_input('Miles',0)
     miles = int(m)
     
-    p = st.number_input('Price (For valuation leave at 0)',0)
+    p = st.number_input('Price (For personal valuation leave at 0)',0)
     price = int(p)
     
     y = []
@@ -89,7 +95,7 @@ elif mode == 'Manual entry':
         
     vec = st.selectbox('Body Type',v)
     
-    hp = st.number_input('Horsepower (for electric vehicles leave at 0)',0)
+    hp = st.number_input('Horsepower (For electric vehicles leave at 0. \nFor all other fuel types the modal power for that type will be chosen)',0)
     
     #appending all in order to make array for program use
     data.append(make)
@@ -103,4 +109,28 @@ elif mode == 'Manual entry':
     data.append(tran)
     data.append(fuel)
     
+    click = st.button('Enter')
     
+    if click:
+        if data[9] != 'Electric' and data[7] == 0:
+            q = db.query(f"""SELECT Horsepower, COUNT(*) as num
+	                        FROM (SELECT Horsepower 
+	                            FROM car_data 
+	                            WHERE make = '{data[0]}' 
+	                            AND model = '{data[1]}' 
+	                            AND engine_size = {data[6]} 
+	                            AND fuel_type = '{data[9]}')
+                            GROUP BY Horsepower
+                            ORDER BY num DESC
+                            LIMIT 1""")
+            data[7] = int(q.iloc[0]['horsepower'])
+            
+        '''to avoid any potential issues with my university i will not be doing all of the 
+        validation as i believe this should be enough to avoid any problems
+        if you want it to be more robust then u will need to implement the rest. its very simple
+        to do and you have an example one above. Finding the mode is the easiest way without needing
+        re-input'''
+        
+        pred = prediction(data)
+        
+        st.write(data)
---
+++ valuate.py
@@ -0,0 +1,37 @@
+import joblib
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pandas as pd
+
+#model
+rf = joblib.load('RF_Model.joblib')
+
+#encoders
+make_enc = joblib.load('Encoders/make_classes.joblib')
+model_enc = joblib.load('Encoders/model_classes.joblib')
+vec_type_enc = joblib.load('Encoders/vec_type_classes.joblib')
+tran_type_enc = joblib.load('Encoders/tran_type_classes.joblib')
+fuel_type_enc = joblib.load('Encoders/fuel_type_classes.joblib')
+
+def prediction(data):
+    
+    make = make_enc.transform(data[0])
+    model = model_enc.tranform(data[1])
+    year = pd.to_numeric(data[3])
+    vec = vec_type_enc.transform(data[4])
+    miles = pd.to_numeric(data[5])
+    es = pd.to_numeric(data[6])
+    hp = pd.to_numeric(data[7])
+    tran = tran_type_enc.transform(data[8])
+    fuel = fuel_type_enc.transform(data[9])
+    
+    v = [make,model,year,vec,miles,es,hp,tran,fuel]
+    
+    vehicle = np.array(v, dtype=object)
+    
+    p_price = rf.predict([vehicle])
+    
+    price_arr = [data[2],p_price]
+    
+    return price_arr
